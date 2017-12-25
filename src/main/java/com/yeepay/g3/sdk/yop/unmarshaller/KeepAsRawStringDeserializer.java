11b962ddda37cc50da852d3247f279c418f210cc
张文康 <wenkang.zhang@yeepay.com>
2017-12-25 16:48:29 +0800

    feat: jackson 2.2.2 -> 2.8.9

--- src/main/java/com/yeepay/g3/sdk/yop/unmarshaller/KeepAsRawStringDeserializer.java
+++ src/main/java/com/yeepay/g3/sdk/yop/unmarshaller/KeepAsRawStringDeserializer.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.io.StringReader;
-import java.lang.reflect.Field;
 
 /**
  * title: 保留Json对象为raw string的形式，不尝试解析它<br/>
@@ -22,36 +19,21 @@ import java.lang.reflect.Field;
  */
 public class KeepAsRawStringDeserializer extends JsonDeserializer<String> {
 
-    protected static final Logger LOGGER = Logger.getLogger(KeepAsRawStringDeserializer.class);
-
     @Override
     public String deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-        if (JsonToken.START_OBJECT != jp.getCurrentToken()) {
-            throw new IllegalArgumentException("KeepAsRawStringDeserializer can't be used here,current token is not a start of json object!");
+        if (JsonToken.START_OBJECT != jp.getCurrentToken() && JsonToken.START_ARRAY != jp.getCurrentToken()) {
+            throw new IllegalArgumentException("KeepAsRawStringDeserializer can't be used here,current token is not a start of json object or json array!");
         }
 
+        Object sourceRef = jp.getCurrentLocation().getSourceRef();
+        if (!(sourceRef instanceof String)) {
+            throw new IllegalArgumentException("source ref of json is not an instance of String,can't use KeepAsRawStringDeserializer here!");
+        }
+
+        String rawJson = (String) sourceRef;
         int startLocation = (int) jp.getCurrentLocation().getCharOffset();
         jp.skipChildren();
         int endLocation = (int) jp.getCurrentLocation().getCharOffset();
-
-        StringReader sr = (StringReader) jp.getInputSource();
-        return getStringViaReflection(sr).substring(startLocation, endLocation + 1);
-    }
-
-    /**
-     * StringReader -> String
-     * 没有通过StringReader的接口来获取String，是因为读取StringReader会改变对象本身的状态，可能会造成其它影响
-     *
-     * @return
-     */
-    private String getStringViaReflection(StringReader reader) {
-        try {
-            Field strField = StringReader.class.getDeclaredField("str");
-            strField.setAccessible(true);
-            return (String) strField.get(reader);
-        } catch (Exception e) {
-            LOGGER.error("error when get str from StringReader via reflection", e);
-            return null;
-        }
+        return rawJson.substring(startLocation - 1, endLocation);
     }
 }
