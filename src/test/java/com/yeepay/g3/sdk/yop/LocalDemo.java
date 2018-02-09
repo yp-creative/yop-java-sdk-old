c25a6b8211d72be28124d688a56259ccd18ad543
baitao.ji <dreambt@gmail.com>
2018-02-09 11:15:25 +0800

    fix: 优化文件上传逻辑

--- src/test/java/com/yeepay/g3/sdk/yop/LocalDemo.java
+++ src/test/java/com/yeepay/g3/sdk/yop/LocalDemo.java
@@ -34,12 +34,6 @@ public class LocalDemo {
     private static final String[] APP_KEYS = {"yop-boss", "app_smtpZIFHFtrvLd9BR", "yop-boss"};
     private static final String[] APP_SECRETS = {"PdZ74F6sxapgOWJ31QKmYw==", "Iw4WPHO6hhjmOt7AoBP7HWWmdg6RMRsT6xZElc4RIAA=", "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCXsKWWClznZbdTwp9183e4Ygu/twbQhuS6LPpu/TZ+OFwwauvIZnOyKu+rFh6apKyVxiLEkssnTsBjLjUIlypEGU2SdLGkswWAPvVdunLjjWEz37W2w4VNkGf8bGCQ9fIxMynoBCTeeWcQz896e1y2p5YZHygUhXGLM/9q5mr3iQQgrEPdFEAdlfLexkbVIF2bS02NsDFLNvqKNk7219cefxWPgJfN7RukUIZyy4nbeevbMAAFpNUFh1NlAh4qzwocOfbZ3NgtwJDf29jibpM3dacS7tqYGwpeGpKazS9tZgTAYcX2kLT7s+G6vVzVQR61pvvDs5ubyfsw/KFR8KDDAgMBAAECggEAShSE6Z+p+4AbZhaYVbxPbYbEgh5af6BBOAMbUvTqlf3kV+j/uWD/g7WgUod87r0ZZBPdiu69tDarkkRQth9NDvDkh2/iCbM8LoOQxPN3hFXZcMICNn2KLnUls4siJelXHFwGTT8o2lWj1fwHMaPphXKWxTIIGu2IpBkC1iwtdTF8mqe2HH+H2djBE96JXVZIf3/FgGu8ppmXa/xG4DfrTxFnGEJzgaadT3Z+ybXbqjYgFgmmBnZOaTx1XPQfLGQVYJz9BunDhwhrqBUM+QuLr1jUsMsj/Yud52cNXjwq9z8FfkKUdVVfE4VrzH8JpKKk7Vim7RWBQER29jlEnV+ysQKBgQDjMWxZz4AveXxWSx7MgXN9PEzxzmGWSApseDskSi5PAmXa4ut5XyNJUiGJ8Zf+cssPfWFNtB7suJBuoMTtrQSap2tgoo70y7QSO0ZlZ0v5Ny9LYh8oHvDgBJVNmS5HWv1U1/VHxNHczNmQ05smXNo1bzMYe5Xo10J2W47UUTgOHwKBgQCq7G6B5RfD+O1jdmYWlilh5oi1XGdYJGnzhs9DmAUN5plQ3VxpUFxxQCgOwXCskfT9QUVYhsIpQIs2iCylwuNDuxxiEQyRpeBirRaqmxvosv08Trwsr1Vs/Cuh17ZZOS+OUehN0fDZCiruK4e2btVfv8LlE1KMuoiUsn1X2gWQ3QKBgCyqBrcRSA4NQBhm5EMoH+A6/pV7EUxOFV6FtHrJ6pi1y/hgLBLMVU+Qye8og80OHEWLTJnOE1ZOYnadPJnNLd6Jk16IFrqhYWFELe65hAIWi0GypJVqn8gqnn+G4cY9aRhI7HuTgf56dzs1nobIMk3W8qCZizsfNn22OjobTX3ZAoGBAJsTusvF1IMs5g05DjTt9wvpQx3xgZ46I5sdNA3q7qMHFxGEVeUDUWw7Plzs61LXdoUU5FsGoUEWW3iVopSett3r9TuQpmu7KVO+IXOXGYJOa259LUQJrKMeRGQpuDtJpDknXXLFyRTSodLH0fEWrCecb7KxjlM6ptLrAshjemtNAoGBAMzGo6aNER8VZfET8Oy0i5G8aVBp6yrMiQsNOj4S1VPoHI+Pc6ot5rDQdjek9PRzF9xeCU4K7+KLaOs6fVmTfsFpPbDafCTTmos9LGr5FIyXpU7LQCl3QPHWPDd5ezsu9SPVjzsEPX3WTSOJuUA8hE7pJnAzMHLGAFpIXJRu3Z/y"};
 
-    // DEV
-    private final String BASE_URL = "http://127.0.0.1:8064/yop-center/";
-
-    // QA
-//    private final String BASE_URL = "http://10.151.30.88:8064/yop-center/";
-
     @BeforeClass
     public static void setUp() throws Exception {
         System.setProperty("yop.sdk.config.file", "/config/yop_sdk_config_dev.json");
@@ -49,7 +43,7 @@ public class LocalDemo {
     @Test
     public void testAES_SHA1() throws Exception {
         int i = 0;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.setSignAlg("sha1");
         request.addParam("requestFlowId", "test123456");//请求流水标识
         request.addParam("name", "张文康");
@@ -67,7 +61,7 @@ public class LocalDemo {
     @Test
     public void testAES_SHA256() throws Exception {
         int i = 0;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.setSignAlg("sha-256");
         request.addParam("corpName", "安徽四创电子股份有限公司青海分公司");//企业名称
         request.addParam("regNo", "630104063035716");//工商注册号
@@ -85,7 +79,7 @@ public class LocalDemo {
     @Test
     public void testAES256_SHA256() throws Exception {
         int i = 1;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.setSignAlg("sha-256");
         request.addParam("corpName", "安徽四创电子股份有限公司青海分公司");//企业名称
         request.addParam("regNo", "630104063035716");//工商注册号
@@ -103,7 +97,7 @@ public class LocalDemo {
     @Test
     public void testCreateToken() throws IOException {
         int i = 0;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.setSignAlg("SHA-256");
 
         request.addParam("grant_type", "password");//请求流水标识
@@ -117,7 +111,7 @@ public class LocalDemo {
 
     @Test(timeout = 5000)
     public void testUpLoadFileOld() throws IOException, URISyntaxException {
-        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0]);
         request.addParam("fileType", "IMAGE");
 //        request.addParam("_file", "file:/Users/xxx/1.png");
         request.addParam("_file", "src/test/resources/log4j.xml");
@@ -128,7 +122,7 @@ public class LocalDemo {
 
     @Test(timeout = 5000)
     public void testUpLoadFileNew1() throws IOException, URISyntaxException {
-        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0]);
         request.addParam("fileType", "IMAGE");
 
         request.addFile("src/test/resources/log4j.xml");
@@ -139,7 +133,7 @@ public class LocalDemo {
 
     @Test(timeout = 5000)
     public void testUpLoadFileNew2() throws IOException, URISyntaxException {
-        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0]);
         request.addParam("fileType", "IMAGE");
 
         request.addFile(new File("src/test/resources/log4j.xml"));
@@ -150,7 +144,7 @@ public class LocalDemo {
 
     @Test(timeout = 5000)
     public void testUpLoadFileNew3() throws IOException, URISyntaxException {
-        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0]);
         request.addParam("fileType", "IMAGE");
 
         request.addFile(new FileInputStream(new File("src/test/resources/log4j.xml")));
@@ -161,7 +155,7 @@ public class LocalDemo {
 
     @Test(timeout = 50000)
     public void testUpLoadFileNew4() throws IOException, URISyntaxException {
-        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0]);
         request.setSignAlg("SHA-256");
         request.addParam("corp_id", "356e1dc1-4c11-419b-a043-cccb537dfb9b");
         request.addParam("user_name", "qian.li");
@@ -173,7 +167,7 @@ public class LocalDemo {
         YopResponse response = YopClient.post("/rest/v2.0/hbird/oauth2/token", request);
         AssertUtils.assertYopResponse(response);
 
-        YopRequest request2 = new YopRequest(APP_KEYS[0], APP_SECRETS[0], BASE_URL);
+        YopRequest request2 = new YopRequest(APP_KEYS[0], APP_SECRETS[0]);
         request2.addParam("target_id", "010b3c35-f384-493f-919d-67177f70dda7");
         request2.addParam("target_type", "USER");
 //        request2.addParam("fileBase64", Base64.encodeBase64String(IOUtils.toByteArray(new FileInputStream(new File("/Users/dreambt/test2.txt")))));
@@ -189,7 +183,7 @@ public class LocalDemo {
 
     @Test
     public void testRsa() throws Exception {
-        YopRequest request = new YopRequest(BASE_URL);
+        YopRequest request = new YopRequest();
         request.addParam("corpName", "安徽四创电子股份有限公司青海分公司");//企业名称
         request.addParam("regNo", "630104063035716");//工商注册号
         request.addParam("requestCustomerId", "yop-boss");//子商户编号
@@ -203,7 +197,7 @@ public class LocalDemo {
     @Test
     public void testRsa2() throws Exception {
         int i = 2;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.addParam("corpName", "安徽四创电子股份有限公司青海分公司");//企业名称
         request.addParam("regNo", "630104063035716");//工商注册号
         request.addParam("requestCustomerId", "yop-boss");//子商户编号
@@ -217,7 +211,7 @@ public class LocalDemo {
     @Test
     public void testRsaUploadFile() throws IOException {
         int i = 2;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.addParam("fileType", "IMAGE");
 //        request.addParam("_file", "file:/Users/dreambt/xuekun-3.pfx");
         request.addParam("_file", "src/test/resources/log4j.xml");
@@ -228,7 +222,7 @@ public class LocalDemo {
     @Test(timeout = 5000)
     public void testRsaUploadFileNew1() throws IOException {
         int i = 2;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.addParam("fileType", "IMAGE");
 
         request.addFile("src/test/resources/log4j.xml");
@@ -240,7 +234,7 @@ public class LocalDemo {
     @Test(timeout = 5000)
     public void testRsaUploadFileNew2() throws IOException {
         int i = 2;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.addParam("fileType", "IMAGE");
 
         request.addFile(new File("src/test/resources/log4j.xml"));
@@ -252,7 +246,7 @@ public class LocalDemo {
     @Test(timeout = 5000)
     public void testRsaUploadFileNew3() throws IOException {
         int i = 2;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.addParam("fileType", "IMAGE");
 
         request.addFile(new FileInputStream(new File("src/test/resources/log4j.xml")));
@@ -265,40 +259,42 @@ public class LocalDemo {
     @Test
     public void testLoadClass() throws Exception {
         int i = 0;
-        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+        YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i]);
         request.addParam("className", "com.yeepay.g3.facade.ymf.facade.laike.OrderFacade");//这个写YOP就可以了
 
         YopResponse response = YopClient.post("/rest/v1.0/system/loader/methods", request);
         AssertUtils.assertYopResponse(response);
     }
 
-    @Ignore
     @Test
-    public void testQueryAccount() throws InterruptedException {
-        int i = 0;
-        final YopRequest request = new YopRequest(APP_KEYS[i], APP_SECRETS[i], BASE_URL);
+    public void testBackendLatency() throws IOException {
+        YopRequest request = new YopRequest(APP_KEYS[0], APP_SECRETS[0]);
+        request.setSignAlg("SHA-256");
+        request.addParam("backendLatency", "1000");
 
-        request.addParam("customerNo", "10012481831");
-        request.addParam("customerNdso", "10012481831");
+        YopResponse response = YopClient.post("/rest/v1.0/test/mock-proxy/backend-latency", request);
+        AssertUtils.assertYopResponse(response);
+    }
 
-        int N = 1;
+    //    @Test
+    public void testBatch() throws InterruptedException {
+        int N = 1000;
         final CountDownLatch latch = new CountDownLatch(N);
-        for (i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    YopResponse response = null;
                     try {
-                        response = YopClient.post("/rest/v1.0/member/queryAccount", request);
+                        testBackendLatency();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    System.out.println(response.toString());
                     latch.countDown();
                 }
             }).start();
         }
         latch.await();
-//        AssertUtils.assertYopResponse(response);
+
+        Thread.sleep(999999L);
     }
 }
