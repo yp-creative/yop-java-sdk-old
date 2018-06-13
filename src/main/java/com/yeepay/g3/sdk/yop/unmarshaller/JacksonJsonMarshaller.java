dc4614782a3098040eb9e6697479c810a1c5c5ff
menghao.chen <menghao.chen@yeepay.com>
2018-06-13 19:13:22 +0800

    fix: jackson默认数字反序列化为BigDecimal，保持精度（目前丢失精度导致再次序列化为字符串后的返回结果验签失败）

--- src/main/java/com/yeepay/g3/sdk/yop/unmarshaller/JacksonJsonMarshaller.java
+++ src/main/java/com/yeepay/g3/sdk/yop/unmarshaller/JacksonJsonMarshaller.java
@@ -2,6 +2,7 @@ package com.yeepay.g3.sdk.yop.unmarshaller;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
@@ -31,6 +32,7 @@ public class JacksonJsonMarshaller {
         objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
         objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        objectMapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
         objectMapper.setSerializationInclusion(Include.NON_EMPTY);
         objectMapper.setAnnotationIntrospector(new JacksonAnnotationIntrospector());
     }
